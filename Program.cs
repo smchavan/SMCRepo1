3145d5fc2f348af2b13acf21f1736875212e2e94
smchavan <smchavan@users.noreply.github.com>
2019-05-02 09:47:20 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,23 +7,17 @@ namespace Mod2_Lab1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            // Request user input with ReadLine()
+           
             Console.WriteLine("Please enter an integer value and press Enter.");
 
-            // Assign the entered value to the variable input
-            // convert input to integer before using
+            
             int input = Int32.Parse(Console.ReadLine());
 
-            // Check to see if the number is even.
-            // Here we use the simple task of checking for a remainder when dividing by 2
-            // The (%) or modulus operator returns the remainder of integer devision.
-            // If the remainder is 0, then the value is able to be divided by 2 with
-            // no remainder, which means it is an even number.
             if (input % 2 == 0)
             {
                 Console.WriteLine("The entered number was an even number.");
             }
-            else  // the remainder was not 0 so the value entere is an odd number.
+            else  
             {
                 Console.WriteLine("The entered number was not an even number.");
             }
